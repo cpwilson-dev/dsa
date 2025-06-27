80b17bb59271bb36333e6450c6f74b219b0897b4
Chris Wilson <cpwilson87@outlook.com>
2025-06-27 11:28:45 +0100

    feat: add SSG pages

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Data Structures & Algorithms
 
+## Plan:
+- Name: dsa (needs work)
+- Idea: Build a SSG taking markdown notes, displaying in a TUI style. Include a fzf search similar to telescope.
+- Time: 1 month
+- Notes:
+    - Design => https://wallenart.dev/
+    - Theme => https://lawsofux.com/
+- TODO:
+    - Add Types
+    - Build out template and style sheets
+    - Update markdown parser to create index & content
+    - Clean out tutorial notes
+    - Test with DSA notes
+    - Write up from DSA book 
+    - Test Github pages
+    - Integrate FZF search
+
+
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
+import os
+import sys
+import shutil
 
-from textnode import TextNode, TextType
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
+
 
 def main():
-    node = TextNode("hello",TextType.LINK, "http://google.com")
-    print(node)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print("Setup public directory")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Generating static assets")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating pages...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+
 
 main()
